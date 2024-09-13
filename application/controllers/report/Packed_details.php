54f20dd7d620a9f0d99a0c37cd8e5301972fdb7a
sutouch08 <sutouch08@gmail.com>
2024-09-13 21:26:00 +0700

    add set_time_limit to report

--- application/controllers/report/Packed_details.php
+++ application/controllers/report/Packed_details.php
@@ -29,6 +29,11 @@ class Packed_details extends PS_Controller
 
 	public function get_report()
 	{
+		ini_set('memory_limit','512M'); // This also needs to be increased in some cases. Can be changed to a higher value as per need)
+    ini_set('sqlsrv.ClientBufferMaxKBSize','524288'); // Setting to 512M
+    ini_set('pdo_sqlsrv.client_buffer_max_kb_size','524288'); // Setting to 512M - for pdo_sqlsrv
+		set_time_limit(600);
+		
 		$sc = TRUE;
 		$ds = array();
 
@@ -95,6 +100,11 @@ class Packed_details extends PS_Controller
 
 	public function do_export()
 	{
+		ini_set('memory_limit','512M'); // This also needs to be increased in some cases. Can be changed to a higher value as per need)
+    ini_set('sqlsrv.ClientBufferMaxKBSize','524288'); // Setting to 512M
+    ini_set('pdo_sqlsrv.client_buffer_max_kb_size','524288'); // Setting to 512M - for pdo_sqlsrv
+		set_time_limit(600);
+
 		$allUser = $this->input->post('allUser') == 1 ? TRUE : FALSE;
 		$users = $this->input->post('users');
 		$selectDate = $this->input->post('selectDate');
